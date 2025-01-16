a3814cb6cedcf1b175ca0f4c105969012a025027
Henok <hdbeleive@gmail.com>
2025-01-16 18:21:50 +0300

    final for task 0

--- quiz.js
+++ quiz.js
@@ -1,3 +1,15 @@
 function checkAnswer(){
     const correctAnswer="4";
+
+    const userAnswer=document.querySelector('input[name="quiz"]:checked')?.value;
+
+    const feedback=document.getElementById("feedback");
+
+    if (userAnswer===correctAnswer){
+        feedback.textContent="correct! Well Done."
+    }else {
+       feedback.textContent="Thats Incorrect! Please try again."
+    }
+
 }
+document.getElementById("submit-answer").addEventListener('click' , checkAnswer);
